43a2d43f3d87220a2778d903eec70dc40814a77b
masterabhi <meabhishek7196@gmail.com>
2018-01-09 21:44:37 +0530

    Main activity start

--- app/src/main/java/com/example/abhishek/mymusic/MainActivity.java
+++ app/src/main/java/com/example/abhishek/mymusic/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.abhishek.mymusic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button register, log_in;
+    EditText First_Name, Last_Name, Email, Password ;
+    String F_Name_Holder, L_Name_Holder, EmailHolder, PasswordHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
