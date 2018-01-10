05a5397996fdbf9a7216c44c98cb74df4fe93309
masterabhi <meabhishek7196@gmail.com>
2018-01-10 22:57:58 +0530

    Main activity start

--- app/src/main/java/com/example/abhishek/mymusic/MainActivity.java
+++ app/src/main/java/com/example/abhishek/mymusic/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.abhishek.mymusic;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
     Button register, log_in;
     EditText First_Name, Last_Name, Email, Password ;
     String F_Name_Holder, L_Name_Holder, EmailHolder, PasswordHolder;
+    String HttpURL = "https://androidjsonblog.000webhostapp.com/User/UserRegistration.php";
+    Boolean CheckEditText ;
+    ProgressDialog progressDialog;
+    HashMap<String,String> hashMap = new HashMap<>();
+    //HttpParse httpParse = new HttpParse();
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
